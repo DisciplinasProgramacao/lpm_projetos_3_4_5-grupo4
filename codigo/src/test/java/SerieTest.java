f4c6f7a2c4410afa7bf83919604782ce00498492
gabriel <gabrielaugustosb13@gmail.com>
2023-04-12 22:02:21 -0300

    test

--- codigo/src/test/java/SerieTest.java
+++ codigo/src/test/java/SerieTest.java
@@ -1,14 +1,20 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SerieTest {
 
     private Serie serie;
     @Test
-    void testGerarItemsDoJson()  {
+    void testVerificaAudiencia()  {
         serie = new Serie("a volta dos que não foram", "comédia", "pt-br", 3);
         serie.registrarAudiencia();
         assertEquals(1, serie.getAudiencia());
+    }
+
+    @Test
+    void testInstanciaSerie(){
+        assertThrows(Error.class, () -> new Serie("a volta dos que não foram", "comédia", "pt-br", 1));
     }
 }
