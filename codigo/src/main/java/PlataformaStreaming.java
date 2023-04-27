28583f84b0c0f97ded5c26eba1bd1dfc4aadaf1e
Vinicius Lima <thelimavinicius@gmail.com>
2023-04-26 22:04:56 -0300

    add PlataformaStreaming

---
+++ codigo/src/main/java/PlataformaStreaming.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class PlataformaStreaming {
+    private String nome;
+    private HashSet<Media> midias;
+    private HashSet<Cliente> clientes;
+    private Cliente clienteAtual;
+
+    public Cliente login(String nomeUsuario, String senha) {
+        Optional<Cliente> cliente = clientes.stream().filter(c -> c.getNomeUsuario().equals(nomeUsuario) && c.getSenha().equals(senha)).findFirst();
+        clienteAtual = cliente.orElse(null);
+
+        return clienteAtual;
+    }
+
+    public void adicionarMidia(Media midia) {
+        midias.add(midia);
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public List<Media> filtrarPorGenero(String genero) {
+        return midias.stream().filter(m -> m.getGenero().equals(genero)).collect(Collectors.toList());
+    }
+
+    public List<Media> filtrarPorIdioma(String idioma) {
+        return midias.stream().filter(m -> m.getIdioma().equals(idioma)).collect(Collectors.toList());
+    }
+
+    public List<Media> filtrarPorQtdEpisodios(int quantEpisodios) {
+        return midias.stream().filter(m -> m instanceof Serie && ((Serie) m).getQuantidadeEpisodios().equals(quantEpisodios)).collect(Collectors.toList());
+
+    }
+    public Media buscarMidia(String nome) {
+        Optional<Media> midia = midias.stream().filter(m -> m.getNome().equals(nome)).findFirst();
+
+        return midia.orElse(null);
+    }
+
+    public void registrarAudiencia(String nomeMidia) {
+        Media midia = buscarMidia(nomeMidia);
+        if (midia != null) midia.registrarAudiencia();
+    }
+
+    public void logoff() {
+        clienteAtual = null;
+    }
+}
