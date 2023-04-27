6a2099d5d1990f97d9a29f5341a0ad89d38e2845
NiloCan <titolianchen3341@gmail.com>
2023-04-26 22:07:32 -0300

    Upload classe filme e geradorDeFilme

--- codigo/src/main/java/Filme.java
+++ codigo/src/main/java/Filme.java
@@ -1,25 +1,16 @@
-import java.sql.Date;
 
-public class Filme extends Media {
-
-    private Integer duracao;
-    private Date dataLancamento;
-
-    public Filme(String nome, String genero, String idioma, Integer duracao, Date dataLancamento) {
-        super(nome, genero, idioma);
-        this.dataLancamento = dataLancamento;
-        this.duracao = duracao;
-    }
-
-    public Integer getDuracao() {
-        return duracao;
-    }
-
-    public Date getDataLancamento() {
-        return dataLancamento;
-    }
-
-    public Integer passaDuracaoParaSegundos() {
-        return duracao * 60;
-    }
+public class Filme {
+	private int duracao;
+	private String id, nome, dataLancamento;
+	
+	public Filme(String[] dadosLidos) {
+		this.id = dadosLidos[0];
+		this.nome = dadosLidos[1];
+		this.dataLancamento = dadosLidos[2];
+		this.duracao = Integer.parseInt(dadosLidos[3]);
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
 }
---
+++ codigo/src/main/java/GeradorDeFilme.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Paths;
+
+
+
+public class GeradorDeFilme {
+ private static String arquivo;
+ 
+ public GeradorDeFilme(String enderecoArquivo) {
+	 this.arquivo = enderecoArquivo; 
+ }
+ public static List<Filme> gerarFilmes() throws IOException {
+	 List<Filme> listaFilmes = new ArrayList<Filme>();
+	 try {
+			List<String> allLines = Files.readAllLines(Paths.get(arquivo));
+
+			for (String line : allLines) {
+				listaFilmes.add(new Filme(line.split(";")));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+ return listaFilmes;
+ }
+}
