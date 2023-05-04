f6e2461d64b85d66ceff04664fe72798a655d18e
Rafael Pierre Martins <1406184@sga.pucminas.br>
2023-05-03 22:13:21 -0300

    add: metodo cadastra filme

--- codigo/src/main/java/Filme.java
+++ codigo/src/main/java/Filme.java
@@ -5,11 +5,13 @@ public class Filme extends Media {
 
     private Integer duracao;
     private Date dataLancamento;
+    private Integer id;
 
-    public Filme(String nome, String genero, String idioma, Integer duracao, Date dataLancamento) {
+    public Filme(String nome, String genero, String idioma, Integer duracao, Date dataLancamento, Integer id) {
         super(nome, genero, idioma);
         this.dataLancamento = dataLancamento;
         this.duracao = duracao;
+        this.id = id;
     }
     
     public Filme(String[] dadosLidos) {
@@ -29,21 +31,30 @@ public class Filme extends Media {
         return duracao * 60;
     }
 
-    public void CadastraFilmeClientSide(){
+    /**
+     * 
+     */
+    public String cadastraFilmeClientSide(){
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Seja bem-vindo ao cadastro de Filmes!");
         System.out.println("Digite o Id do filme:");
-        int idFilme = sc.nextInt();
+        Integer idFilme = sc.nextInt();
         System.out.println("Digite o Nome do filme:");
-        String nomeFilme = sc.nextLine();
+        String nomeFilme = sc.next();
         System.out.println("Digite a data de lancamento do filme:");
-        String dataLancamento = sc.nextLine();
+        String dataLancamentoFilme = sc.next();
         System.out.println("Digite a duracao do filme:");
-        int duracaoFilme = sc.nextInt();
-    
-         
+        Integer duracaoFilme = sc.nextInt();
+        System.out.println("Digite o genero do filme:");
+        String generoFilme = sc.next();
+        System.out.println("Digite o idioma do filme:");
+        String idiomaFilme = sc.next();
+        sc.close();
 
+        String stringSalvaInformacoes = idFilme + ";" + nomeFilme + ";" + dataLancamentoFilme + ";" + duracaoFilme + ";" + generoFilme + ";" + idiomaFilme; 
+        
+        return stringSalvaInformacoes;
     }
 
     
--- codigo/src/main/java/Main.java
+++ codigo/src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main{
 
     public static void main(String[] args) {
-        
+      Filme filme = new Filme(null, null, null, null, null, null);
+
+      filme.cadastraFilmeClientSide();
     }
+
 }
