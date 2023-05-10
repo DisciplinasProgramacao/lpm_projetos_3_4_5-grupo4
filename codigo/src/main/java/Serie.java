96b98319ed920aec70e5a480e7a3fff91896f18c
gabriel <gabrielaugustosb13@gmail.com>
2023-05-10 20:07:29 -0300

    docs: adiciona java doc parte 2

--- codigo/src/main/java/Serie.java
+++ codigo/src/main/java/Serie.java
@@ -1,40 +1,70 @@
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Scanner;
 
 public class Serie extends Media implements Serializable {
     private Integer quantidadeEpisodios;
 
+    /**
+     * Construtor da classe Serie.
+     *
+     * @param nome               o nome da série
+     * @param genero             o gênero da série
+     * @param idioma             o idioma da série
+     * @param quantidadeEpisodios a quantidade de episódios da série
+     * @throws Error se a quantidade de episódios for menor que 2
+     */
     public Serie(String nome, String genero, String idioma, Integer quantidadeEpisodios) {
         super(nome, genero, idioma);
         if (quantidadeEpisodios < 2) {
-            throw new Error("Precisa ter no minimo 2 episodios");
+            throw new Error("Precisa ter no mínimo 2 episódios");
         }
         this.quantidadeEpisodios = quantidadeEpisodios;
     }
 
+    /**
+     * Obtém a quantidade de episódios da série.
+     *
+     * @return a quantidade de episódios da série
+     */
     public Integer getQuantidadeEpisodios() {
         return quantidadeEpisodios;
     }
 
+    /**
+     * Salva todas as séries em um arquivo.
+     *
+     * @param allSeries a lista de todas as séries a serem salvas
+     * @throws IOException caso ocorra um erro durante o processo de salvamento
+     */
     public static void salvarTodasSeries(List<Serie> allSeries) throws IOException {
         GenericDao<Serie> serieDao = new GenericDao<>();
         serieDao.save(allSeries, "series.dat");
     }
 
+    /**
+     * Carrega todas as séries de um arquivo.
+     *
+     * @return a lista de todas as séries carregadas do arquivo
+     * @throws IOException            caso ocorra um erro durante o processo de carregamento
+     * @throws ClassNotFoundException caso a classe da série não seja encontrada durante o processo de carregamento
+     */
     public static List<Serie> carregarTodasSeries() throws IOException, ClassNotFoundException {
         GenericDao<Serie> serieDao = new GenericDao<>();
         return serieDao.load("series.dat");
     }
 
-        public String geraDadosSerie() {
+    /**
+     * Gera os dados da série.
+     *
+     * @return uma string com os dados da série no formato "idSerie;nome;quantidadeEpisodios"
+     */
+    public String geraDadosSerie() {
+        String nome = this.getNome();
+        int qtdeEpisodios = this.getQuantidadeEpisodios();
+        int idSerie = gerarId();
+        String dadosSerie = String.format("%d;%s;%d", idSerie, nome, qtdeEpisodios);
 
-            String nome = this.getNome();
-            int qtdeEpisodios = this.getQuantidadeEpisodios();
-            int idSerie = gerarId();
-            String dadosSerie = String.format("%d;%s;%d", idSerie, nome, qtdeEpisodios);
-
-            return dadosSerie;
-        }
+        return dadosSerie;
     }
+}
