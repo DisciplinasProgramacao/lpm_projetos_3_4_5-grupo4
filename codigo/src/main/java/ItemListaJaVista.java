5175505584f03c343bfd19b908778f41ba2df389
Nilo <titolianchen3341@gmail.com>
2023-05-17 19:48:44 -0300

    Adição de verificação de especialista na classe cliente, com seus testes

---
+++ codigo/src/main/java/ItemListaJaVista.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+
+public class ItemListaJaVista {
+    
+    private Media listaJaVistas;
+    private Date dataVizualizacao;
+
+    public ItemListaJaVista(Media media, Date dataa){
+        this.listaJaVistas = media;
+        this.dataVizualizacao = dataa;
+    }
+
+    public boolean isValid(){
+        Date hoje = new Date();
+        long umDiaEmMilissegundos = 24 * 60 * 60 * 1000; // Quantidade de milissegundos em um dia
+        long trintaDiasEmMilissegundos = 30 * umDiaEmMilissegundos; // Quantidade de milissegundos em 30 dias
+        if(dataVizualizacao.getTime() >= (hoje.getTime() - trintaDiasEmMilissegundos)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Media getMedia(){
+        return this.listaJaVistas;
+    }
+
+    public Date getDataVizualizacao(){
+        return this.dataVizualizacao;
+    }
+
+}
